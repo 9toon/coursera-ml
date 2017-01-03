990b58c4e69f9aa0d3041b73052a98062a0d5804
Yusuke Naito <yusuke-naito@cookpad.com>
2017-01-03 17:49:12 +0900

    Solve Logistic Regression Cost

--- machine-learning-ex2/ex2/costFunction.m
+++ machine-learning-ex2/ex2/costFunction.m
@@ -7,7 +7,7 @@ function [J, grad] = costFunction(theta, X, y)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 grad = zeros(size(theta));
 
@@ -21,10 +21,8 @@ grad = zeros(size(theta));
 %
 
 
-
-
-
-
+h = sigmoid(X * theta);
+J = 1 / m * (-y' * log(h) - (1 - y)' * log(1 - h));
 
 
 % =============================================================
