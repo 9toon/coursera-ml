167d06bb1150830b14f73a4944e8ca2a110407f5
Yusuke Naito <yusuke-naito@cookpad.com>
2017-01-03 19:14:33 +0900

    Regularized Logistic Regression Gradient

--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -23,6 +23,8 @@ h = sigmoid(X * theta);
 
 J = 1 / m * (-y' * log(h) - (1 - y)' * log(1 - h)) + lambda / (2 * m) * sum(theta(2:end) .^ 2);
 
+grad(1) = 1 / m * (h - y)' * X(:, 1);
+grad(2:end) = (1 / m * (h - y)' * X(:, 2:end))' + (lambda / m) * theta(2:end);
 
 % =============================================================
 
