48c80a0e082453ba7eb6f8c4c3f980c953643da9
Yusuke Naito <yusuke-naito@cookpad.com>
2017-01-29 01:37:59 +0900

    Solve Regularized Gradient

--- machine-learning-ex4/ex4/nnCostFunction.m
+++ machine-learning-ex4/ex4/nnCostFunction.m
@@ -112,6 +112,8 @@ Theta2_grad = 1 / m * Theta2_grad;
 
 
 
+Theta1_grad(:, 2:end) = Theta1_grad(:, 2:end) + lambda / m * Theta1(:, 2:end);
+Theta2_grad(:, 2:end) = Theta2_grad(:, 2:end) + lambda / m * Theta2(:, 2:end);
 
 
 
