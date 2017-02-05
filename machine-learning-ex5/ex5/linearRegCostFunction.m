f098407779c7e88d6b5ecd735f7c3a64ae597eb8
Yusuke Naito <yusuke-naito@cookpad.com>
2017-02-06 02:18:13 +0900

    Solve Regularized Linear Regression Gradient

--- machine-learning-ex5/ex5/linearRegCostFunction.m
+++ machine-learning-ex5/ex5/linearRegCostFunction.m
@@ -21,12 +21,10 @@ grad = zeros(size(theta));
 
 
 h = X * theta;
+J = 1 / (2 * m) * (sum((h -y) .^ 2)) + ( (lambda / (2 * m)) * (sum((theta(2:end) .^ 2))));
 
-J = 1 / (2 * m) * (sum((h - y) .^ 2)) + ( (lambda / (2 * m)) * (sum((theta(2:end) .^ 2))) );
-
-
-
-
+grad(1) = 1 / m * (h - y)' * X(:, 1);
+grad(2:end) = (1 / m * (h - y)' * X(:, 2:end))' + (lambda / m) * theta(2:end);
 
 
 
